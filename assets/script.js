98422b5824589687277440ddb1e5e894527ec222
Luidson Lima Santos <luidsonlsti@gmail.com>
2024-05-06 23:19:31 -0300

    implementa página de projetos, muda tema do toggler do darkmode e melhora estilização

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,7 @@ const toggleDarkMode = () => {
     aplyDarkMode()
 };
 
+//Função responsável por alterar as variáveis do css caso o tema esuro esteja ativado
 const aplyDarkMode = ()=>{
     let darkModeEnabled = localStorage.getItem('darkMode');
 
@@ -29,17 +30,20 @@ const aplyDarkMode = ()=>{
         document.documentElement.style.setProperty('--header-background', '#111');
         document.documentElement.style.setProperty('--main-background', '#333');
         document.documentElement.style.setProperty('--main-text-color', '#fff');
+        document.documentElement.style.setProperty('--toggler-background', 'linear-gradient(to bottom, var(--main-background) 0%, #000000 50%, #000 50%, #000044 100%)');
     }else{
         document.documentElement.style.setProperty('--dark-mode-toggler-icon', 'url("sun-svgrepo-com.png")');
         document.documentElement.style.setProperty('--header-background', '#333');
         document.documentElement.style.setProperty('--main-background', '#fff');
         document.documentElement.style.setProperty('--main-text-color', '#000');
+        document.documentElement.style.setProperty('--toggler-background', 'linear-gradient(to bottom, var(--main-background) 0%, #6fc0e8 50%, #6fc0ff 50%, #0000ff 100%)');
     }
 
 
     
 }
 
+// Aplica o modo escuro ao carregar a página
 window.onload = ()=>{
     aplyDarkMode()
 }
